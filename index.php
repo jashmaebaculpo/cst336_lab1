c2a669fb23ad7f38a0a90cd17277c7090ccdda75
Jashmae Baculpo <jbaculpo@csumb.edu>
2018-02-05 07:27:57 +0000

    changed index.html to index.php

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset=”utf-8” />
+            
+        <title> 
+            Jashmae Baculpo Personal Website
+        </title>
+        
+        <style>
+            body
+            {
+                background-color: #80B7BA !important;
+            }
+        </style>
+        
+        <link href = "css/styles.css" rel = "stylesheet" type = "text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <h1>Jashmae Baculpo </h1>
+        </header>
+        
+        <nav>
+            <hr width = "50%"/>
+            <a href = "index.html"> Home</a>
+            <a href = "about.html"> About</a>
+            <a href = "contact.html"> Contact</a>
+        </nav>
+        
+        <br /> <br />
+        <figure id = "me">
+            <img src = "/Portfolio-lab/img/Jashmae_B.png" alt "Picture of Jashmae Baculpo." />
+        </figure>
+        
+        <main>
+            <div id = "welcomeText" >
+                Hello! <br />
+                <p>
+                    Thank you for visiting my professional portfolio website.
+                </p>
+                <p>
+                    I am a Software Engineer working towards obtaining a 
+                </p>
+                <p>
+                    Bachelors Degree in Computer Science from California State University of Monterey Bay.
+                </p>
+                <p>
+                    Feel free to contact me!
+                </p>
+            </div>
+        
+        </main>
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            <hr>
+            <p>
+                <img src="/Portfolio-lab/img/CSUMB-logo.png"></img>
+                <strong> Disclaimer: </strong>
+            This is used for academic purposes only.
+            </p>
+            CST 336: Internet Programming. Spring 2018. Baculpo.
+
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
